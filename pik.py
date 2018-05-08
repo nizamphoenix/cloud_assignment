fc4c5050b0974e4ace226d717b92591d0bd9e866
nizamphoenix <nizamphoenix@gmail.com>
2018-05-08 13:35:42 +1000

    added

---
+++ pik.py
@@ -0,0 +1,331 @@
+{
+  'entities': {
+    'hashtags': [
+      {
+        'text': 'ThisIsAustralia',
+        'indices': [
+          0,
+          16
+        ]
+      },
+      {
+        'text': 'FirstNations',
+        'indices': [
+          17,
+          30
+        ]
+      },
+      {
+        'text': 'ausmedia',
+        'indices': [
+          31,
+          40
+        ]
+      },
+      {
+        'text': 'ausbiz',
+        'indices': [
+          41,
+          48
+        ]
+      },
+      {
+        'text': 'sydneywritersfestival',
+        'indices': [
+          49,
+          71
+        ]
+      }
+    ],
+    'urls': [
+      {
+        'display_url': 'twitter.com/nyunggai/statu…',
+        'url': 'https://t.co/i0XTjCWapq',
+        'indices': [
+          72,
+          95
+        ],
+        'expanded_url': 'https://twitter.com/nyunggai/status/991561116215001088'
+      }
+    ],
+    'symbols': [
+
+    ],
+    'user_mentions': [
+
+    ]
+  },
+  'quote_count': 0,
+  'favorited': "False",
+  'in_reply_to_status_id': "None",
+  '_rev': '1-bd90dfd714a5bc8a023c0bb5b884aee8',
+  '_id': 991562243404779520,
+  'id': 991562243404779520,
+  'coordinates': "None",
+  'quoted_status': {
+    'entities': {
+      'hashtags': [
+
+      ],
+      'urls': [
+
+      ],
+      'symbols': [
+
+      ],
+      'media': [
+        {
+          'media_url': 'http://pbs.twimg.com/media/DcK7kGzU0AAD7X6.jpg',
+          'url': 'https://t.co/7VaI2FDeFZ',
+          'sizes': {
+            'medium': {
+              'resize': 'fit',
+              'w': 901,
+              'h': 1200
+            },
+            'large': {
+              'resize': 'fit',
+              'w': 1538,
+              'h': 2048
+            },
+            'small': {
+              'resize': 'fit',
+              'w': 511,
+              'h': 680
+            },
+            'thumb': {
+              'resize': 'crop',
+              'w': 150,
+              'h': 150
+            }
+          },
+          'media_url_https': 'https://pbs.twimg.com/media/DcK7kGzU0AAD7X6.jpg',
+          'id': 991561096950501376,
+          'display_url': 'pic.twitter.com/7VaI2FDeFZ',
+          'type': 'photo',
+          'indices': [
+            62,
+            85
+          ],
+          'id_str': '991561096950501376',
+          'expanded_url': 'https://twitter.com/nyunggai/status/991561116215001088/photo/1'
+        }
+      ],
+      'user_mentions': [
+
+      ]
+    },
+    'quote_count': 0,
+    'favorited': "False",
+    'in_reply_to_status_id': "None",
+    'id': 991561116215001088,
+    'coordinates': "None",
+    'reply_count': 1,
+    'truncated': "False",
+    'id_str': '991561116215001088',
+    'in_reply_to_user_id': "None",
+    'extended_entities': {
+      'media': [
+        {
+          'media_url': 'http://pbs.twimg.com/media/DcK7kGzU0AAD7X6.jpg',
+          'url': 'https://t.co/7VaI2FDeFZ',
+          'sizes': {
+            'medium': {
+              'resize': 'fit',
+              'w': 901,
+              'h': 1200
+            },
+            'large': {
+              'resize': 'fit',
+              'w': 1538,
+              'h': 2048
+            },
+            'small': {
+              'resize': 'fit',
+              'w': 511,
+              'h': 680
+            },
+            'thumb': {
+              'resize': 'crop',
+              'w': 150,
+              'h': 150
+            }
+          },
+          'media_url_https': 'https://pbs.twimg.com/media/DcK7kGzU0AAD7X6.jpg',
+          'id': 991561096950501376,
+          'display_url': 'pic.twitter.com/7VaI2FDeFZ',
+          'type': 'photo',
+          'indices': [
+            62,
+            85
+          ],
+          'id_str': '991561096950501376',
+          'expanded_url': 'https://twitter.com/nyunggai/status/991561116215001088/photo/1'
+        }
+      ]
+    },
+    'is_quote_status': "False",
+    'in_reply_to_status_id_str': "None",
+    'favorite_count': 1,
+    'user': {
+      'profile_text_color': '333333',
+      'profile_background_color': 'C0DEED',
+      'id': 741819608,
+      'is_translator': "False",
+      'location': 'Sydney',
+      'follow_request_sent': "None",
+      'translator_type': 'none',
+      'time_zone': 'Brisbane',
+      'profile_use_background_image':"True",
+      'utc_offset': 36000,
+      'url': 'http://www.nyunggablack.com',
+      'profile_sidebar_border_color': 'C0DEED',
+      'profile_background_image_url': 'http://abs.twimg.com/images/themes/theme1/bg.png',
+      'default_profile_image': "False",
+      'following': "None",
+      'profile_image_url': 'http://pbs.twimg.com/profile_images/897619816986451969/RAEDoluJ_normal.jpg',
+      'notifications': "None",
+      'followers_count': 18685,
+      'listed_count': 180,
+      'profile_background_image_url_https': 'https://abs.twimg.com/images/themes/theme1/bg.png',
+      'geo_enabled':"True",
+      'lang': 'en',
+      'profile_image_url_https': 'https://pbs.twimg.com/profile_images/897619816986451969/RAEDoluJ_normal.jpg',
+      'profile_sidebar_fill_color': 'DDEEF6',
+      'friends_count': 1399,
+      'contributors_enabled': "False",
+      'created_at': 'Tue Aug 07 00:58:42 +0000 2012',
+      'default_profile':"True",
+      'statuses_count': 27728,
+      'id_str': '741819608',
+      'description': 'Business advisor. Host of Mundine Means Business. Author of Warren Mundine - In Black & White. Bundjalung Australian. Gumbaynggirr, Yuin, Irish descent.',
+      'protected': "False",
+      'verified':"True",
+      'favourites_count': 7359,
+      'name': 'Nyunggai Warren Mundine AO',
+      'profile_background_tile': "False",
+      'profile_link_color': '1DA1F2',
+      'screen_name': 'nyunggai',
+      'profile_banner_url': 'https://pbs.twimg.com/profile_banners/741819608/1517624504'
+    },
+    'source': '<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>',
+    'lang': 'en',
+    'created_at': 'Wed May 02 06:12:47 +0000 2018',
+    'in_reply_to_user_id_str': "None",
+    'display_text_range': [
+      0,
+      61
+    ],
+    'possibly_sensitive': "False",
+    'text': 'Ready for my conversation, Sydney Writers Festival, tomorrow. https://t.co/7VaI2FDeFZ',
+    'place': "None",
+    'contributors': "None",
+    'geo': "None",
+    'retweet_count': 0,
+    'retweeted': "False",
+    'filter_level': 'low',
+    'in_reply_to_screen_name': "None"
+  },
+  'truncated': "False",
+  'quoted_status_id': 991561116215001088,
+  'display_text_range': [
+    0,
+    71
+  ],
+  'in_reply_to_user_id': "None",
+  'quoted_status_id_str': '991561116215001088',
+  'is_quote_status':"True",
+  'in_reply_to_status_id_str': "None",
+  'favorite_count': 0,
+  'user': {
+    'profile_text_color': '333333',
+    'profile_background_color': '9AE4E8',
+    'id': 65607720,
+    'is_translator': "False",
+    'location': 'Melbourne, Australia',
+    'follow_request_sent': "None",
+    'translator_type': 'none',
+    'time_zone': 'Melbourne',
+    'profile_use_background_image':"True",
+    'utc_offset': 36000,
+    'url': 'http://www.andrewheslop.com',
+    'profile_sidebar_border_color': 'FFFFFF',
+    'profile_background_image_url': 'http://pbs.twimg.com/profile_background_images/504880456852987904/n_-0NFEM.jpeg',
+    'default_profile_image': "False",
+    'following': "None",
+    'profile_image_url': 'http://pbs.twimg.com/profile_images/864019349459263488/3LbfGL2p_normal.jpg',
+    'notifications': "None",
+    'followers_count': 35725,
+    'listed_count': 392,
+    'profile_background_image_url_https': 'https://pbs.twimg.com/profile_background_images/504880456852987904/n_-0NFEM.jpeg',
+    'geo_enabled':"True",
+    'lang': 'en',
+    'profile_image_url_https': 'https://pbs.twimg.com/profile_images/864019349459263488/3LbfGL2p_normal.jpg',
+    'profile_sidebar_fill_color': 'DDFFCC',
+    'friends_count': 6260,
+    'contributors_enabled': "False",
+    'created_at': 'Fri Aug 14 10:31:11 +0000 2009',
+    'default_profile': "False",
+    'statuses_count': 113140,
+    'id_str': '65607720',
+    'description': 'Keynote Speaker • Award-winning Social Entrepreneur • Commentator • MC • Australia Day Ambassador • NSW Volunteer of Year • Neighbour Day founder #BeInspired',
+    'protected': "False",
+    'verified':"True",
+    'favourites_count': 22359,
+    'name': 'Andrew Heslop',
+    'profile_background_tile': "False",
+    'profile_link_color': '0084B4',
+    'screen_name': 'AndrewHeslop',
+    'profile_banner_url': 'https://pbs.twimg.com/profile_banners/65607720/1496219963'
+  },
+  'source': '<a href="http://twitter.com/#!/download/ipad" rel="nofollow">Twitter for iPad</a>',
+  'lang': 'und',
+  'in_reply_to_screen_name': "None",
+  'reply_count': 0,
+  'timestamp_ms': '1525241835820',
+  'created_at': 'Wed May 02 06:17:15 +0000 2018',
+  'filter_level': 'low',
+  'id_str': '991562243404779520',
+  'possibly_sensitive': "False",
+  'text': '#ThisIsAustralia #FirstNations #ausmedia #ausbiz #sydneywritersfestival https://t.co/i0XTjCWapq',
+  'place': {
+    'country_code': 'AU',
+    'country': 'Australia',
+    'url': 'https://api.twitter.com/1.1/geo/id/3f14ce28dc7c4566.json',
+    'name': 'Australia',
+    'bounding_box': {
+      'coordinates': [
+        [
+          [
+            112.921114,
+            -43.740482
+          ],
+          [
+            112.921114,
+            -9.142176
+          ],
+          [
+            159.109219,
+            -9.142176
+          ],
+          [
+            159.109219,
+            -43.740482
+          ]
+        ]
+      ],
+      'type': 'Polygon'
+    },
+    'place_type': 'country',
+    'full_name': 'Australia',
+    'id': '3f14ce28dc7c4566',
+    'attributes': {
+
+    }
+  },
+  'contributors': "None",
+  'geo': "None",
+  'retweet_count': 0,
+  'retweeted': ,
+  'in_reply_to_user_id_str': "None"
+}
